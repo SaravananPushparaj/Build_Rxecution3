03b9230356561a2133691ff4b671ad3b276facb1
unknown <saravananpushparaj@gmail.com>
2015-09-19 07:19:43 +0530

    v1.0

---
+++ src/test/java/Day1/Locators.java
@@ -0,0 +1,17 @@
+package Day1;
+
+import org.testng.annotations.Test;
+
+public class Locators {
+	
+	@Test(groups="{SmokeTest}")
+	
+	public void first_test()
+	{
+		
+		System.out.println("Hello world");
+		
+		
+	}
+
+}
